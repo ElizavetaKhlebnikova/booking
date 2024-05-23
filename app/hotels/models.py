0a3a8c60cdec94b79aa1b8f49687fae64ba44a07
Elizaveta Khlebnikova <elizavetakhlebnikova@gmail.com>
2024-05-23 23:09:21 +0300

    Moved the rooms model

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,5 +1,5 @@
 from app.database import Base
-from sqlalchemy import Column, Integer, String, JSON
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
 
 class Hotels(Base):
     __tablename__ = 'hotels'
@@ -9,4 +9,16 @@ class Hotels(Base):
     location = Column(String, nullable=False)
     services = Column(JSON)
     room_quantity = Column(Integer, nullable=False)
+    image_id = Column(Integer)
+
+class Rooms(Base):
+    __tablename__ = 'rooms'
+
+    id = Column(Integer, primary_key=True)
+    hotel_id = Column(ForeignKey('hotels.id'), nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    price = Column(Integer, nullable=False)
+    services = Column(JSON, nullable=True)
+    quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
